adc2c69967c28715aed9dbbdc5f9f5eff0c92332
Вихарев Алексей <a.v.vikharev@gmail.com>
2022-11-15 18:31:58 +0300

    commit

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -1,8 +1,6 @@
 from django_filters import rest_framework as filter
 from django_filters.rest_framework import FilterSet, filters
 
-from rest_framework.filters import SearchFilter
-
 from recipes.models import Recipe, Tag, Ingredient
 
 
