99a56e8b5c7521128ca85706d4da4a7ebd678a0d
Вихарев Алексей <a.v.vikharev@gmail.com>
2022-11-15 21:15:14 +0300

    commit

--- backend/api/views.py
+++ backend/api/views.py
@@ -8,7 +8,6 @@ from djoser.views import UserViewSet
 
 from rest_framework import status, viewsets
 from rest_framework.decorators import action
-from rest_framework.filters import IngredientFilter
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.status import HTTP_400_BAD_REQUEST
@@ -17,7 +16,7 @@ from recipes.models import (Favorite, Ingredient, IngredientRecipe, Recipe,
                             ShoppingCart, Tag)
 from users.models import Follow, User
 
-from .filters import RecipeFilter
+from .filters import IngredientFilter, RecipeFilter
 from .pagination import CustomPagination
 from .permissions import IsAuthorOrAdminOrReadOnly
 from .serializers import (CreateRecipeSerializer, FavoriteSerializer,
